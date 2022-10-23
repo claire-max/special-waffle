86f9e022f072f011eb46fa6e1ebde06f99dfe58a
ClaireFree <claire@robingoodfoods.com>
2022-10-23 14:33:56 -0700

    All tests have passed

--- src/template.js
+++ src/template.js
@@ -99,7 +99,7 @@ module.exports = team => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading bg-danger">
+            <div class="col-12 jumbotron mb-3 team-heading bg-primary">
                 <h1 class="text-center text-white">My Team</h1>
             </div>
         </div>
